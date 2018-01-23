f547e01fe2fb05f327eaed31513d2848d59f0cf7
Shabbir Haider <shabbir1993@users.noreply.github.com>
2018-01-23 23:39:36 +0500

    ex-1-12.c

---
+++ Chapter-1/ex-1-12.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main () 
+{
+	int c;
+	int state = OUT; 
+	while((c=getchar())!=EOF)
+	{
+		if(c == ' ' || c == '\n' || c == '\t')
+		{
+			state = OUT;
+			c = '\n';
+			putchar(c);
+		}
+			
+		else
+		{
+			state = IN;
+			putchar(c);
+		}
+		
+	}
+	return 0; 
+}
